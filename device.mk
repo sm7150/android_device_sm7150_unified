1a62b63b36db7732ce1aa872ab1fec79f7d5e865
Michael Benedict <michaelbt@live.com>
2020-03-15 22:14:30 +1100

    r5q: add overlay
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,11 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
+
 # No A/B
 AB_OTA_UPDATER := false
 
@@ -29,7 +34,8 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.qcom.rc
 
 # Recovery
 PRODUCT_PACKAGES += \
