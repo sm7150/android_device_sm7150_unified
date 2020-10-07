185443cdb7293e8128c45944cb4af64e8638bc1b
priyesh2 <priyesh241997@gmail.com>
2020-10-07 03:26:36 +0000

    initialise new realise tools

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -14,12 +14,27 @@
 # limitations under the License.
 
 import common
+import re
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.input_zip)
+  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+
+def AddBootloaderAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-bootloader\s*=\s*(\d)$', android_info)
+  if m:
+    bootloader_version = m.group(1)
+    cmd = ('assert(a70q.verify_bootloader("{}") == "1" || abort("ERROR: This package '
+           'requires bootloader version 5 or older. Please wait until your '
+           'firmware is supported."););'.format(bootloader_version))
+    info.script.AppendExtra(cmd)
 
 def AddImage(info, basename, dest):
   path = "IMAGES/" + basename
